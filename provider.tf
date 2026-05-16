e469c9371a88eef6aa8aae2039bf6996e972406e
manu.s.mendon@ahanait.com <manoj.mendon@ahanait.com>
2026-05-16 17:30:37 +0530

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "d6e440b8-7002-4ce9-9448-6a5d8158716e"
+}
